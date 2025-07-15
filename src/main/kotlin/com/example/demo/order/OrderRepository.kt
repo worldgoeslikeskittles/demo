494b74c1a81517f7f4121a488401a7e14a4af7e3
pavel <thechasercloud@mail.ru>
2025-07-16 01:56:29 +0300

    initial state for demo

--- src/main/kotlin/com/example/demo/order/OrderRepository.kt
+++ src/main/kotlin/com/example/demo/order/OrderRepository.kt
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import java.util.*
 
 interface OrderRepository : JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
-
-    @EntityGraph(attributePaths = ["customer", "payment", "shipment", "orderItems.product"])
-    override fun findById(id: Long): Optional<Order>
 }
